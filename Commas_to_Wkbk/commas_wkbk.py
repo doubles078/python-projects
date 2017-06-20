b6d370772fd9878529d89c220b37ca56692a3ca2
Dan Donohue <dan.donohue@EF.com>
2017-06-20 11:27:51 -0400

    Commas project fix

--- Commas_to_Wkbk/commas_wkbk.py
+++ Commas_to_Wkbk/commas_wkbk.py
@@ -1,11 +1,10 @@
 import openpyxl
 
-
-
-wb = openpyxl.load_workbook('randomnums.xlsx')
+wb = openpyxl.load_workbook('adding_commas.xlsx')
 sheet = wb.get_sheet_by_name('randomnums')
+output_sheet = wb.create_sheet('randomnums_with_commas')
 
-for i in range(1, 255):
-    sheet.cell(row=i, column=1).value = str(sheet.cell(row=i, column=1).value) + ', '
+for i in range(1, 200):
+    output_sheet.cell(row=i, column=1).value = str(sheet.cell(row=i, column=1).value) + ', '
 
-wb.save("comma_output.xlsx")
+wb.save("adding_commas.xlsx")
