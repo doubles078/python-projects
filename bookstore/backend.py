2ba51e5ceeee0b4d1ae0dd37e865274ad7d8591b
Dan Donohue <dan.donohue@EF.com>
2017-06-26 16:17:29 -0400

    OOP Tutorial

--- bookstore/backend.py
+++ bookstore/backend.py
@@ -1,57 +1,40 @@
 import sqlite3
 
-def connect():
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title text, author text, year INTEGER, isbn INTEGER)")
-    conn.commit()
-    conn.close()
+class Database:
 
-def insert(title, author, year, isbn):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    #By inserting a null, python knows that the primary key should be incremented automatically
-    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
-    conn.commit()
-    conn.close()
+    def __init__(self):
+        self.conn=sqlite3.connect("books.db")
+        self.cur=self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title text, author text, year INTEGER, isbn INTEGER)")
+        self.conn.commit()
 
-def view():
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM book")
-    #we want the data returned as a tuple
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def insert(self, title, author, year, isbn):
+        #By inserting a null, python knows that the primary key should be incremented automatically
+        self.cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
+        self.conn.commit()
 
-#Pass empty strings as default values so a user can choose to pass only one argument
-def search(title="", author="", year="", isbn=""):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title=? or author=? or year=? or isbn=?", (title, author, year, isbn))
-    #we want the data returned as a tuple
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        #we want the data returned as a tuple
+        rows=self.cur.fetchall()
+        return rows
 
-def delete(id):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    #Need a comma after if only one argument
-    cur.execute("DELETE FROM book WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
+    #Pass empty strings as default values so a user can choose to pass only one argument
+    def search(self, title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM book WHERE title=? or author=? or year=? or isbn=?", (title, author, year, isbn))
+        #we want the data returned as a tuple
+        rows=self.cur.fetchall()
+        return rows
 
-def update(id, title, author, year, isbn):
-    conn=sqlite3.connect("books.db")
-    cur=conn.cursor()
-    #Need a comma after if only one argument
-    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn, id))
-    conn.commit()
-    conn.close()
+    def delete(self, id):
+        #Need a comma after if only one argument
+        self.cur.execute("DELETE FROM book WHERE id=?", (id,))
+        self.conn.commit()
 
-connect()
-insert('Welcome to the Monkey House', 'Kurt Vonnegut', 1944, 1123232)
-delete(1)
-print(view())
-print(search(author="Stephen King"))
+    def update(self, id, title, author, year, isbn):
+        #Need a comma after if only one argument
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
