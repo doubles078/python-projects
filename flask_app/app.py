15954289a3e022feb08d1962d7f493a8e507a825
Dan Donohue <dan.donohue@EF.com>
2017-06-30 14:58:49 -0400

    Flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,15 +1,41 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_collector'
+db=SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_=db.Column(db.String(120), unique=True)
+    height_=db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_=email_
+        self.height_=height_
+
+
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/success", methods=['POST'])
 def success():
-    return render_template("success.html")
+    if request.method=='POST':
+        email=request.form["email_name"]
+        height=request.form["height_cm"]
+        send_email(email, height)
+        if db.session.query(Data).filter(Data.email_==email).count() == 0:
+            data=Data(email,height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        return render_template('index.html', text="  <div class='alert alert-warning'>Seems like we've got something from that email address already.</div>")
 
 if __name__ == '__main__':
     app.debug=True
