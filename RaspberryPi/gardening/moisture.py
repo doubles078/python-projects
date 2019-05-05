307c78d3f02384ae6e983dbbedd8b83e70567ed2
doubles078 <ddonohue@owneriq.com>
2019-05-05 15:23:50 -0400

    fix: removing the nonworking script

--- RaspberryPi/gardening/moisture.py
+++ RaspberryPi/gardening/moisture.py
@@ -1,28 +1,25 @@
-#Make sure you turn on SPI first
-#“8 Advanced Options” -> “A6 SPI” -> “Yes”.
-#!/usr/bin/python
- 
-import spidev
-import os
-import time
- 
-delay = 0.2
- 
-spi = spidev.SpiDev()
-spi.open(0,0)
- 
-def readChannel(channel):
-  val = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((val[1]&3) << 8) + val[2]
+# Importing modules
+import spidev # To communicate with SPI devices
+from numpy import interp  # To scale values
+from time import sleep  # To add delay
+
+
+# Start SPI connection
+spi = spidev.SpiDev() # Created an object
+spi.open(0,0) 
+
+
+# Read MCP3008 data
+def analogInput(channel):
+  spi.max_speed_hz = 1350000
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
   return data
- 
-if __name__ == "__main__":
-  try:
-    while True:
-      val = readChannel(0)
-      if (val != 0):
-        print(val)
-      time.sleep(delay)
-      
-  except KeyboardInterrupt:
-    print "Cancel."
+
+
+while True:
+  output = analogInput(0) # Reading from CH0
+  output = interp(output, [0, 1023], [100, 0])
+  output = int(output)
+  print("Moisture:", output)
+  sleep(0.1)
